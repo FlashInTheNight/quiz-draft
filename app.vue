a3d29ca34f172249b81ab5961e4119efd2e98c97
unknown <example@gmail.com>
2023-07-18 23:25:52 +0200

    first commit

---
+++ app.vue
@@ -0,0 +1,11 @@
+<template>
+  <main class="bg-slate-800 w-screen h-screen flex justify-center items-center">
+    <div class="h-[600px] w-[600px] border-2 rounded-2xl border-stone-100 drop-shadow-2xl shadow-slate-100">
+       
+    </div>
+  </main>
+</template>
+
+<script setup>
+
+</script>
